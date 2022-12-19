e05d021dcae810184f06ae4756c6efbcbd2e409a
JeminButani <95534964+JeminButani@users.noreply.github.com>
2022-12-19 13:38:51 +0530

    first commit

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import FocusedStatusBar from "./FocusedStatusBar";
+import { CircleButton, RectButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
+
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  RectButton,
+  CircleButton,
+  DetailsBid,
+  DetailsDesc,
+  SubInfo,
+};
